813ffb01b3dab064497e156145794239bab66e4c
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-06-01 17:43:16 -0300

    feat: Adicionando TesteNivelamento

---
+++ src/main/java/com/cognito/entity/TesteNivelamento.java
@@ -0,0 +1,57 @@
+package com.cognito.entity;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class TesteNivelamento {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private int id_teste;
+  private int id_usuario;
+  private String nivel_detectado;
+  private Timestamp data_realizacao;
+
+  public TesteNivelamento(int id_teste, int id_usuario, String nivel_detectado, Timestamp data_realizacao) {
+    this.id_teste = id_teste;
+    this.id_usuario = id_usuario;
+    this.nivel_detectado = nivel_detectado;
+    this.data_realizacao = data_realizacao;
+  }
+
+  public int getId_teste() {
+    return id_teste;
+  }
+
+  public void setId_teste(int id_teste) {
+    this.id_teste = id_teste;
+  }
+
+  public int getId_usuario() {
+    return id_usuario;
+  }
+
+  public void setId_usuario(int id_usuario) {
+    this.id_usuario = id_usuario;
+  }
+
+  public String getNivel_detectado() {
+    return nivel_detectado;
+  }
+
+  public void setNivel_detectado(String nivel_detectado) {
+    this.nivel_detectado = nivel_detectado;
+  }
+
+  public Timestamp getData_realizacao() {
+    return data_realizacao;
+  }
+
+  public void setData_realizacao(Timestamp data_realizacao) {
+    this.data_realizacao = data_realizacao;
+  }
+}
