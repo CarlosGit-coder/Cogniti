c229306f0107d7ebe5d2b840973537301458bf7d
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-06-01 20:32:59 -0300

    feat: Começanod atualização para nova sintaxe. esperando dql

--- src/main/java/com/cognito/repository/UsuarioRepository.java
+++ src/main/java/com/cognito/repository/UsuarioRepository.java
@@ -1,8 +1,12 @@
 package com.cognito.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.cognito.entity.Usuario;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+  @Query(value = )
 
 }
