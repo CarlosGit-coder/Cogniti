1e35672e08f4a7a7e487276ffd155c6e152cacea
CarlosGit-coder <carlosgabrielmonteiro2006@gmail.com>
2026-06-01 12:31:48 -0300

    Initial commit

---
+++ src/main/java/com/cognito/cogniti/CognitiApplication.java
@@ -0,0 +1,13 @@
+package com.cognito.cogniti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CognitiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cognito/cogniti/CognitiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognito.cogniti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognitiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
